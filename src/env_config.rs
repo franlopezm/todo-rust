c215e965a326a514fd70f340b4586a516c3cd84e
franlopezm <frans.lopez.m@gmail.com>
2024-11-12 16:09:56 +0100

    refactor: Port and host are set by default

--- src/env_config.rs
+++ src/env_config.rs
@@ -2,10 +2,20 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct EnvConfig {
+    #[serde(default = "default_host")]
     pub service_host: String,
+    #[serde(default = "default_port")]
     pub service_port: u16,
 }
 
 pub fn get() -> EnvConfig {
     envy::from_env::<EnvConfig>().expect("SERVICE_HOST and SERVICE_PORT are env vars required.")
+}
+
+fn default_port() -> u16 {
+    8000
+}
+
+fn default_host() -> String {
+    String::from("127.0.0.1")
 }
